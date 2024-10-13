25f5dffe63a4f70286f7e0f2a286458a76ba3ae2
brunolucero19 <brunolucero2419@gmail.com>
2024-10-13 17:53:07 -0300

    Tp integrador hasta video 11

--- main.js
+++ main.js
@@ -1,7 +1,20 @@
 import { setInLocalStorage } from './src/persistence/localstorage'
 import { renderCategories } from './src/services/categories'
+import { handleGetProductsToStore } from './src/views/store'
 import './style.css'
 
+//Aplicacion
+export let categoriaActiva = null
+export const setCategoriaActiva = (categoria) => {
+  categoriaActiva = categoria
+}
+
+export let productoActivo = null
+export const setProductoActivo = (producto) => {
+  productoActivo = producto
+}
+
+handleGetProductsToStore()
 renderCategories()
 
 //Products
@@ -22,14 +35,38 @@ const handleCancelButton = () => {
   closeModal()
 }
 
-const openModal = () => {
+export const openModal = () => {
   const modal = document.getElementById('modalPopUp')
   modal.style.display = 'flex'
+
+  if (productoActivo) {
+    const nombre = document.getElementById('nombre'),
+      imagen = document.getElementById('img'),
+      precio = document.getElementById('precio'),
+      categoria = document.getElementById('categoria')
+    nombre.value = productoActivo.nombre
+    imagen.value = productoActivo.imagen
+    precio.value = productoActivo.precio
+    categoria.value = productoActivo.categoria
+  }
 }
 
-const closeModal = () => {
+export const closeModal = () => {
   const modal = document.getElementById('modalPopUp')
   modal.style.display = 'none'
+  setProductoActivo(null)
+  resetModal()
+}
+
+const resetModal = () => {
+  const nombre = document.getElementById('nombre'),
+    imagen = document.getElementById('img'),
+    precio = document.getElementById('precio'),
+    categoria = document.getElementById('categoria')
+  nombre.value = ''
+  imagen.value = ''
+  precio.value = '0'
+  categoria.value = 'Seleccione una categoria'
 }
 
 //Guardar o modificar elementos
@@ -44,16 +81,26 @@ const handleSaveOrModifyElements = () => {
     imagen = document.getElementById('img').value,
     precio = document.getElementById('precio').value,
     categoria = document.getElementById('categoria').value
-
-  let product = {
-    id: new Date().toISOString(),
-    nombre,
-    imagen,
-    precio,
-    categoria,
+  let product = null
+  if (productoActivo) {
+    product = {
+      ...productoActivo,
+      nombre,
+      imagen,
+      precio,
+      categoria,
+    }
+  } else {
+    product = {
+      id: new Date().toISOString(),
+      nombre,
+      imagen,
+      precio,
+      categoria,
+    }
   }
 
   setInLocalStorage(product)
-
+  handleGetProductsToStore()
   closeModal()
 }
---
+++ src/views/store.js
@@ -0,0 +1,71 @@
+import { setProductoActivo } from '../../main'
+import { handleGetProductLocalStorage } from '../persistence/localstorage'
+import { openModal } from '../../main'
+
+//Funcion para obtener los productos de la tienda y renderizarlos
+export const handleGetProductsToStore = () => {
+  const products = handleGetProductLocalStorage()
+  handleRenderList(products)
+}
+
+//Funcion para renderizar secciones con las categorias de los productos
+export const handleRenderList = (productsIn) => {
+  const burgers = productsIn.filter(
+    (product) => product.categoria === 'Hamburguesas'
+  )
+  const papas = productsIn.filter((product) => product.categoria === 'Papas')
+  const gaseosas = productsIn.filter(
+    (product) => product.categoria === 'Gaseosas'
+  )
+
+  const renderProductGroup = (products, title) => {
+    if (products.length > 0) {
+      const productosHTML = products.map((product, index) => {
+        return `
+                <div class='containerCardItem' id='product-${product.categoria}-${index}'>
+                    <img src="${product.imagen}" alt="imagen">
+                    <div class='containerText'>
+                        <h2>${product.nombre}</h2>
+                        <p class='cardPriceText'><b>Precio: </b> $ ${product.precio}</p>
+                    </div>
+                </div>
+                `
+      })
+      return `
+             <section class='sectionStore'>
+                <h3>${title}</h3>
+                <div class='containerProductStore'>
+                ${productosHTML.join('')}
+                </div>
+            </section>
+            `
+    } else {
+      return ''
+    }
+  }
+
+  //Renderizar cada uno de los productos en la tienda
+  const storeContainer = document.getElementById('storeContainer')
+  storeContainer.innerHTML = `
+        ${renderProductGroup(burgers, 'Hamburguesas')}
+        ${renderProductGroup(papas, 'Papas')}
+        ${renderProductGroup(gaseosas, 'Gaseosas')}
+    `
+
+  //Agregar eventos a los productos
+  const addEvents = (productsIn) => {
+    productsIn.forEach((element, index) => {
+      const productContainer = document.getElementById(
+        `product-${element.categoria}-${index}`
+      )
+      productContainer.addEventListener('click', () => {
+        setProductoActivo(element)
+        openModal()
+      })
+    })
+  }
+
+  addEvents(burgers)
+  addEvents(papas)
+  addEvents(gaseosas)
+}
